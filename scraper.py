3fa91f7f6ab169c5637aa127a9eb76ac3197b634
Ivan Czar <ivanczar2013@gmail.com>
2022-11-07 21:52:42 +1300

    feat: twilio template created

--- scraper.py
+++ scraper.py
@@ -23,25 +23,36 @@ driver.get(URL)
 driver.find_element(By.ID, "username").send_keys(email)
 driver.find_element(By.ID, "password").send_keys(password)
 driver.find_element(By.XPATH, "//div[@class='password-submit']/button").click()
-sleep(5)
+
 
 # nav to markeplace
 driver.find_element(By.LINK_TEXT, "Market").click()
 sleep(5)
 
 # get all listings
+desired = ["gym", "fridge", "console", "pc",
+           "weight", "tv", "phone", "computer", "monitor", "laptop", "television"]
+match = ""
+freeCount = 0
+matchCount = 0
 allListings = driver.find_elements(By.CLASS_NAME, "market-item")
 for listing in allListings:
     title = listing.find_element(
         By.CLASS_NAME, "market-item-subject").text
     price = listing.find_element(
         By.CLASS_NAME, "lozenge").text
+    if "Free" in price:
+        freeCount += 1
+    for item in desired:
+        if item in title.lower():
+            match += f"\t-{title}\n"
+            matchCount += 1
 
-    print("item: " + title + " price: " + price)
 
+# send text
 message = client.messages.create(
     from_='whatsapp:' + os.getenv("FROM_PHONE"),
-    body='test',
+    body=(
+        f'● Found {freeCount} free items \n● Found {matchCount} items matching criteria: {match}'),
     to='whatsapp:' + os.getenv("TO_PHONE")
 )
-sleep(100000)
