6cae5eb4ffa3b466aa664c66cd7c4d675417595a
Ivan Czar <ivanczar2013@gmail.com>
2022-11-07 23:20:03 +1300

    fix: extracted keywords to separate file

---
+++ keywords.py
@@ -0,0 +1,2 @@
+words = ["gym", "fridge", "console", "pc",
+         "weight", "tv", "phone", "computer", "monitor", "laptop", "television", "equipment"]
--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
-from selenium import webdriver
+import os
+from dotenv import load_dotenv
 from time import sleep
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import os
-from dotenv import load_dotenv
 from twilio.rest import Client
+import keywords
 
 load_dotenv()
 URL = os.getenv("URL")
@@ -14,6 +15,10 @@ password = os.getenv("PSWD")
 account_sid = os.getenv("TWILIO_SID")
 auth_token = os.getenv("TWILIO_TOKEN")
 client = Client(account_sid, auth_token)
+keywords = keywords.words
+match = ""
+freeCount = 0
+matchCount = 0
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
@@ -24,17 +29,12 @@ driver.find_element(By.ID, "username").send_keys(email)
 driver.find_element(By.ID, "password").send_keys(password)
 driver.find_element(By.XPATH, "//div[@class='password-submit']/button").click()
 
-
+sleep(5)
 # nav to markeplace
 driver.find_element(By.LINK_TEXT, "Market").click()
 sleep(5)
 
 # get all listings
-desired = ["gym", "fridge", "console", "pc",
-           "weight", "tv", "phone", "computer", "monitor", "laptop", "television"]
-match = ""
-freeCount = 0
-matchCount = 0
 allListings = driver.find_elements(By.CLASS_NAME, "market-item")
 for listing in allListings:
     title = listing.find_element(
@@ -43,11 +43,10 @@ for listing in allListings:
         By.CLASS_NAME, "lozenge").text
     if "Free" in price:
         freeCount += 1
-    for item in desired:
+    for item in keywords:
         if item in title.lower():
             match += f"\t-{title}\n"
             matchCount += 1
-
 
 # send text
 message = client.messages.create(
