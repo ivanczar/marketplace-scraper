7d3756539c20a5d85b2e941d7224723a93d3a03a
Ivan Czar <ivan.czar@uirev.com>
2023-03-29 15:36:04 +1300

    replaced twilio with pywhatkit

--- scraper.py
+++ scraper.py
@@ -5,16 +5,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from twilio.rest import Client
 import keywords
+import pywhatkit
+
 
 load_dotenv()
 URL = os.getenv("URL")
 email = os.getenv("EMAIL")
 password = os.getenv("PSWD")
-account_sid = os.getenv("TWILIO_SID")
-auth_token = os.getenv("TWILIO_TOKEN")
-client = Client(account_sid, auth_token)
+phone = os.getenv("TO_PHONE")
 keywords = keywords.words
 match = ""
 freeCount = 0
@@ -23,7 +22,7 @@ matchCount = 0
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 
-# login
+# # login
 driver.get(URL)
 driver.find_element(By.ID, "username").send_keys(email)
 driver.find_element(By.ID, "password").send_keys(password)
@@ -47,11 +46,7 @@ for listing in allListings:
         if item in title.lower():
             match += f"\t-{title}\n"
             matchCount += 1
-            
-# send text
-message = client.messages.create(
-    from_='whatsapp:' + os.getenv("FROM_PHONE"),
-    body=(
-        f'● Found {freeCount} free items \n● Found {matchCount} items matching criteria: {match}'),
-    to='whatsapp:' + os.getenv("TO_PHONE")
-)
+
+# Only create message object if matchCount != 0 (prevent empty messages)
+pywhatkit.sendwhatmsg_instantly(phone, (
+    f'● Found {freeCount} free items \n● Found {matchCount} items matching criteria: {match}'))
