f142be0a09faf9fbe50cf6e95bd006df3f2e8a1e
Ivan Czar <ivanczar2013@gmail.com>
2023-06-10 21:21:38 +1200

    added email and new listing check

--- scraper.py
+++ scraper.py
@@ -6,51 +6,79 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import keywords
+import yagmail
 
 
 load_dotenv()
 URL = os.getenv("URL")
-email = os.getenv("EMAIL")
-password = os.getenv("PSWD")
-keywords = keywords.words
-match = ""
-freeCount = 0
-matchCount = 0
+TO_EMAIL = os.getenv("TO_EMAIL")
+TO_PSWD = os.getenv("TO_PSWD")
+FROM_EMAIL = os.getenv("FROM_EMAIL")
+FROM_PSWD = os.getenv("FROM_PSWD")
 
+
+# set up driver
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=options)
 
-# # login
+
+keywords = keywords.words
+match = ""
+freeCount = 0
+matchCount = 0
+
+f = open("last_listing.txt", "r")
+lastScrapedTitle = f.read()
+
+# login
 print("Logging in...")
 driver.get(URL)
-driver.find_element(By.ID, "username").send_keys(email)
-driver.find_element(By.ID, "password").send_keys(password)
+driver.find_element(By.ID, "username").send_keys(TO_EMAIL)
+driver.find_element(By.ID, "password").send_keys(TO_PSWD)
 driver.find_element(By.XPATH, "//div[@class='password-submit']/button").click()
 print("Logged in!")
-
 sleep(5)
+
 # nav to markeplace
 print("Navigating to marketplace...")
 driver.find_element(By.LINK_TEXT, "Market").click()
 sleep(5)
 
+
 # get all listings
 print("Scraping listings...")
-allListings = driver.find_elements(By.CLASS_NAME, "market-item")
-for listing in allListings:
-    title = listing.find_element(
-        By.CLASS_NAME, "market-item-subject").text
-    price = listing.find_element(
-        By.CLASS_NAME, "lozenge").text
-    if "Free" in price:
-        freeCount += 1
-    for item in keywords:
-        if item in title.lower():
-            match += f"\t-{title}\n"
-            matchCount += 1
+listingsArr = driver.find_elements(By.CLASS_NAME, "market-item")
+firstListingTitle = listingsArr[0].find_element(By.CLASS_NAME, "market-item-subject").text
+if (lastScrapedTitle != firstListingTitle): #Checks if there are new listings
+    
+    f = open("last_listing.txt", "w")
+    f.write(firstListingTitle)
 
-# Only create message object if matchCount != 0 (prevent empty messages)
-print(f'● Found {freeCount} free items \n● Found {matchCount} items matching criteria: {match}')
+    for listing in listingsArr:
+
+        title = listing.find_element(
+            By.CLASS_NAME, "market-item-subject").text
+        price = listing.find_element(
+            By.CLASS_NAME, "lozenge").text
+        
+        if "Free" in price:
+            freeCount += 1
+        for item in keywords:
+            if item in title.lower():
+                match += f"\t-{title}\n"
+                matchCount += 1
+    # send email
+    if (matchCount != 0):
+        yag = yagmail.SMTP(FROM_EMAIL, FROM_PSWD)
+        contents = [f'● Found {freeCount} free items \n● Found {matchCount} items matching criteria: {match}']
+        yag.send(TO_EMAIL, f'Neighbourly Scrape ({matchCount} matches)', contents)
+        print("Email sent!")
+else:
+    print("No new listings!")
+
+f.close()
+driver.close()
+
