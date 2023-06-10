e6d0b54f662da0717bb7d2f05ca3e402223a4e30
Ivan Czar <ivanczar2013@gmail.com>
2023-06-10 18:59:01 +1200

    implemented for ARM64 arch i.e raspi

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import keywords
 
@@ -22,7 +21,7 @@ options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=options)
 
 # # login
 print("Logging in...")
