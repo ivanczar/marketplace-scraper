c425a77a77f0ba36d1dd3f1599844c3aae18e6ad
Ivan <ivanczar2013@gmail.com>
2024-12-27 16:04:50 +1300

    fix scraper class

--- utils.py
+++ utils.py
@@ -2,10 +2,21 @@ import re
 
 def pluralize(noun):
     if re.search('[sxz]$', noun):
-         return re.sub('$', 'es', noun)
+         return re.sub('$', 'es', noun);
     elif re.search('[^aeioudgkprt]h$', noun):
-        return re.sub('$', 'es', noun)
+        return re.sub('$', 'es', noun);
     elif re.search('[aeiou]y$', noun):
-        return re.sub('y$', 'ies', noun)
+        return re.sub('y$', 'ies', noun);
     else:
-        return noun + 's'
+        return noun + 's';
+    
+def getLastScrapedTitle(filePath):
+    f = open(filePath, "r");
+    lastScrapedTitle = f.read();
+    f.close();
+    return lastScrapedTitle;
+
+def setLastScrapedTitle(filePath, title):
+    f = open(filePath, "w");
+    f.write(title);
+    f.close();
