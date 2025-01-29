dfad6162833daad989d26c6de35b8cf762939163
Ivan Czar <ivan.czar@figured.com>
2025-01-29 21:01:17 +1300

    execute as package

---
+++ marketplace_scraper/__init__.py
@@ -0,0 +1,30 @@
+# marketplace_scraper/__init__.py
+
+import os
+from dotenv import load_dotenv
+from marketplace_scraper.web_driver import Driver
+from marketplace_scraper.scraper import Scraper
+from marketplace_scraper.email_driver import Email
+
+def run_scraper():
+    load_dotenv()
+
+    LOGIN_URL = os.getenv("LOGIN_URL") or ""
+    AUTHENTICATED_URL = os.getenv("AUTHENTICATED_URL") or ""
+    TO_EMAIL = os.getenv("TO_EMAIL") or ""
+    TO_PSWD = os.getenv("TO_PSWD") or ""
+    FROM_EMAIL = os.getenv("FROM_EMAIL") or ""
+    FROM_PSWD = os.getenv("FROM_PSWD") or ""
+
+    webdriver = Driver().driver
+    webscraper = Scraper(webdriver)
+    emailClient = Email(FROM_EMAIL, FROM_PSWD)
+
+    listings = webscraper.scrape(LOGIN_URL, AUTHENTICATED_URL, TO_EMAIL, TO_PSWD)
+
+    if listings.getListingCount():
+        emailClient.send(listings, TO_EMAIL)
+    else:
+        print("No new listings found")
+
+    webdriver.quit()
